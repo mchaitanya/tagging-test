7aed54bd2017a34f4b5130f0809c332f13f1bc97
mchaitanya <chaitanyaaa@gmail.com>
2021-03-18 22:55:03 -0500

    Send HTML file back in response.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express");
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
