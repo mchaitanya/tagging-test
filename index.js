2ce7f5ca13e892d7b4a5915e5b50c54081e93d4e
mchaitanya <chaitanyaaa@gmail.com>
2021-04-06 16:04:29 -0500

    Install GTM container snippet on test page

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
+app.get("/gtm", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "gtm.html"));
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
